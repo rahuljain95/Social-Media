396efdbfe9595c4b97982fb8224cbfe5a6c23128
Rahul Kumar Jain <rahulkumarjain095@gmail.com>
2024-05-14 19:48:52 +0530

    Add Header, Footer and Sidebar component section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sidebar from "./Components/Sidebar";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <>
-      <div>keyword</div>
-    </>
+    <div className="app-container">
+      <Sidebar></Sidebar>
+      <div className="content">
+      <Header></Header>
+      <Footer></Footer>
+      </div>
+    </div>
   );
 }
 
