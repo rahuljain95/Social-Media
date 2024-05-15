9425957dc8283aac6dd8b7d931fc3c74b4c7a66e
Rahul Kumar Jain <rahulkumarjain095@gmail.com>
2024-05-15 10:09:54 +0530

    CreatePost, Post and PostList components are added.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "./Components/Sidebar";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import CreatePost from "./Components/CreatePost";
+import PostList from "./Components/PostList";
 
 function App() {
   return (
     <div className="app-container">
       <Sidebar></Sidebar>
       <div className="content">
-      <Header></Header>
-      <Footer></Footer>
+        <Header></Header>
+        <CreatePost></CreatePost>
+        <PostList></PostList>
+        <Footer></Footer>
       </div>
     </div>
   );
