6ae0cd50c93077d385d5122e476d3371fbcc9269
Rahul Kumar Jain <rahulkumarjain095@gmail.com>
2024-05-18 18:28:35 +0530

    UI design for Home button, delete functionality also done

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,29 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import CreatePost from "./Components/CreatePost";
 import PostList from "./Components/PostList";
+import { useState } from "react";
+import PostListProvider from "./Store/post-list-store";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
   return (
-    <div className="app-container">
-      <Sidebar></Sidebar>
-      <div className="content">
-        <Header></Header>
-        <CreatePost></CreatePost>
-        <PostList></PostList>
-        <Footer></Footer>
+    <PostListProvider>
+      <div className="app-container">
+        <Sidebar
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+        ></Sidebar>
+        <div className="content">
+          <Header></Header>
+          {selectedTab === "Home" ? (
+            <PostList></PostList>
+          ) : (
+            <CreatePost></CreatePost>
+          )}
+          <Footer></Footer>
+        </div>
       </div>
-    </div>
+    </PostListProvider>
   );
 }
 
